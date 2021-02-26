3a9c5e18eab037a77369a730bc17aa5a43d677cd
git config --global user.name Your_Name git config --global user.email Your-Email <ramzankhan1272@gmail.com>
2021-02-26 16:53:07 +0500

    First commit

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+// Types
+import { answerObject } from '../App';
+// Styles
+
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: answerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <Wrapper>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+          key={answer}
+        >
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callback}
+          >
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
+    </div>
+  </Wrapper>
+);
+
+export default QuestionCard;
